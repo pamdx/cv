874bbc27d95669a0f8ba7ffe4e83731c74a9b66c
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2022-08-18 17:33:18 +0200

    Fixed issues related to unavailability of datadrivencv package

--- render_cv.r
+++ render_cv.r
@@ -1,9 +1,5 @@
 # This script builds both the HTML and PDF versions of your CV
-
-# If you wanted to speed up rendering for googlesheets driven CVs you could use
-# this script to cache a version of the CV_Printer class with data already
-# loaded and load the cached version in the .Rmd instead of re-fetching it twice
-# for the HTML and PDF rendering. This exercise is left to the reader.
+# Run this with R version 4.2.0
 
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
